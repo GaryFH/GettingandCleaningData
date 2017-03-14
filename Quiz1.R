3c4ce656c4cc65a72b5c9206ad2b29200c83412d
GaryFH <gary@hulbertbrothers.com>
2017-03-13 18:10:15 -0700

    break doesn't work

--- Quiz1.R
+++ Quiz1.R
@@ -1,6 +1,6 @@
 ##Quiz Problem 1 Getting and Cleaning Data
 
-Get data
+Get 
         url1<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv" 
         dest<-file.path(getwd(),"Quiz1_1")
         download.file(url1,dest)
@@ -10,15 +10,35 @@ Get data
         c2<-c1$VAL[c1$VAL==24&!is.na(c1$VAL)]
         length(c2)
         
+        
+        
+        
+        
 ##   Problem 3 - read rows 18:23 and columns 7:15
         
-        url2<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+        url2<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx "
         dest2<-file.path(getwd(),"Quiz1_2")
         download.file(url2,dest2)
+        date1<-date()
+        print(date1)
+        ##STUFF ABOVE DID NOT WORK?  I downloaded file to my directory and started below
+        
+        
         xcol<-7:15
         yrow<-18:23
-        cc1<-read.xlsx("Quiz1_2",colIndex=xcol,rowIndex=yrow)
+                ##,colIndex=xcol,rowIndex=yrow
+        cc1<-read.xlsx(file="getdata_data_DATA.gov_NGAP.xlsx",sheetIndex = 1,colIndex = xcol,rowIndex = yrow,header = TRUE)
+        
+        sum(cc1$Zip*cc1$Ext,na.rm = T)
         
         
+##   Problem 4  - how many restaurants in zipcode 21231?
         
+        url3<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml" 
+        
+        c22 <- xmlTreeParse(url3,useInternal=TRUE)
+        rootNode<-xmlRoot(c22)
+        xmlName(rootNode)
+        
+        c1<-read.csv("Quiz1_3")
         
