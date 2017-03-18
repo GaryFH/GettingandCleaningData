e9e6d483e5e093edfd1aa477c31b46f48c0beae3
GaryFH <gary@hulbertbrothers.com>
2017-03-18 00:54:08 -0700

    more swirl

--- Swirl work.R
+++ Swirl work.R
@@ -147,4 +147,186 @@ cran %>%
         filter(size_mb <= 0.5) %>%
         arrange(desc(size_mb)) %>% print
 
+# Repeat your calls to gather() and separate(), but this time
+# use the %>% operator to chain the commands together without
+# storing an intermediate result.
+#
+# If this is your first time seeing the %>% operator, check
+# out ?chain, which will bring up the relevant documentation.
+# You can also look at the Examples section at the bottom
+# of ?gather and ?separate.
+#
+# The main idea is that the result to the left of %>%
+# takes the place of the first argument of the function to
+# the right. Therefore, you OMIT THE FIRST ARGUMENT to each
+# function.
+#
+students2 %>%
+        gather(sex_class,count,-grade) %>%
+        separate(sex_class, c("sex", "class")) %>%
+        print
+
+
+## longway step 1 res<-gather(students2,sex_class,count,-grade)
+##         step 2 separate(data=res,col=sex_class, into= c("sex","class")) 
+
+
+
+# Repeat your calls to gather() and separate(), but this time
+# use the %>% operator to chain the commands together without
+# storing an intermediate result.
+#
+# If this is your first time seeing the %>% operator, check
+# out ?chain, which will bring up the relevant documentation.
+# You can also look at the Examples section at the bottom
+# of ?gather and ?separate.
+#
+# The main idea is that the result to the left of %>%
+# takes the place of the first argument of the function to
+# the right. Therefore, you OMIT THE FIRST ARGUMENT to each
+# function.
+#
+students2 %>%
+        gather(sex_class,count,-grade) %>%
+        separate(sex_class, c("sex", "class")) %>%
+        print
+
+
+# Call gather() to gather the columns class1
+# through class5 into a new variable called class.
+# The 'key' should be class, and the 'value'
+# should be grade.
+#
+# tidyr makes it easy to reference multiple adjacent
+# columns with class1:class5, just like with sequences
+# of numbers.
+#
+# Since each student is only enrolled in two of
+# the five possible classes, there are lots of missing
+# values (i.e. NAs). Use the argument na.rm = TRUE
+# to omit these values from the final result.
+#
+# Remember that when you're using the %>% operator,
+# the value to the left of it gets inserted as the
+# first argument to the function on the right.
+#
+# Consult ?gather and/or ?chain if you get stuck.
+#
+students3 %>%
+        gather(class,grade,class1:class5,-name,-test, na.rm= TRUE) %>%
+        print
+
+
+##gather(sex_class,count,-grade)
+
+
+# This script builds on the previous one by appending
+# a call to spread(), which will allow us to turn the
+# values of the test column, midterm and final, into
+# column headers (i.e. variables).
+#
+# You only need to specify two arguments to spread().
+# Can you figure out what they are? (Hint: You don't
+# have to specify the data argument since we're using
+# the %>% operator.
+#
+students3 %>%
+        gather(class, grade, class1:class5, na.rm = TRUE) %>%
+        spread(test,grade) %>%
+        print
+
+##first argument is the variable(test) whose values will become new variable names and
+##the second argument(grade) variable that contains the values going to the new variables
+
+
+# We want the values in the class columns to be
+# 1, 2, ..., 5 and not class1, class2, ..., class5.
+#
+# Use the mutate() function from dplyr along with
+# parse_number(). Hint: You can "overwrite" a column
+# with mutate() by assigning a new value to the existing
+# column instead of creating a new column.
+#
+# Check out ?mutate and/or ?parse_number if you need
+# a refresher.
+#
+students3 %>%
+        gather(class, grade, class1:class5, na.rm = TRUE) %>%
+        spread(test, grade) %>%
+        ### Call to 
+        mutate(class=parse_number(class))  %>%
+        print
+
+
+# Complete the chained command below so that we are
+# selecting the id, name, and sex column from students4
+# and storing the result in student_info.
+#
+student_info <- students4 %>%
+        select(id ,name ,sex ) %>%
+        print
+
+# Add a call to unique() below, which will remove
+# duplicate rows from student_info.
+#
+# Like with the call to the print() function below,
+# you can omit the parentheses after the function name.
+# This is a nice feature of %>% that applies when
+# there are no additional arguments to specify.
+#
+student_info <- students4 %>%
+        select(id, name, sex) %>%
+        unique %>%
+        print
+
+# select() the id, class, midterm, and final columns
+# (in that order) and store the result in gradebook.
+#
+gradebook <- students4 %>%
+        select(id,class,midterm,final) %>%
+        print
+
+
+# Accomplish the following three goals:
+#
+# 1. select() all columns that do NOT contain the word "total",
+# since if we have the male and female data, we can always
+# recreate the total count in a separate column, if we want it.
+# Hint: Use the contains() function, which you'll
+# find detailed in 'Special functions' section of ?select.
+#
+# 2. gather() all columns EXCEPT score_range, using
+# key = part_sex and value = count.
+#
+# 3. separate() part_sex into two separate variables (columns),
+# called "part" and "sex", respectively. You may need to check
+# the 'Examples' section of ?separate to remember how the 'into'
+# argument should be phrased.
+#
+sat %>%  select(-contains("total")) %>%
+        gather(part_sex,count, -score_range) %>%
+        separate(part_sex,c("part","sex")) %>%
+        print
+# Append two more function calls to accomplish the following:
+#
+# 1. Use group_by() (from dplyr) to group the data by part and
+# sex, in that order.
+#
+# 2. Use mutate to add two new columns, whose values will be
+# automatically computed group-by-group:
+#
+#   * total = sum(count)
+#   * prop = count / total
+#
+sat %>%
+        select(-contains("total")) %>%
+        gather(part_sex, count, -score_range) %>%
+        separate(part_sex, c("part", "sex")) %>% 
+        group_by(part,sex) %>%  mutate(total=sum(count),prop=count/total) %>% print
+
+
+
+
+
+
 
